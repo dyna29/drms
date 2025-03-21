768271963c56f7fcd30ab82b3ff74c0f778520b2
dyna29 <dyna29@gmail.com>
2025-03-21 09:11:14 +0530

    Add files via upload

---
+++ wp-salt.php
@@ -0,0 +1,17 @@
+<?php
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ */
+define('AUTH_KEY',         'PFc7L0yu5iMNHCctcJCKBxiJBbHoxK5cLgbAFa3QtsRz4V0pyfT7zK5FjdGP4AJ3');
+define('SECURE_AUTH_KEY',  'L6S2BXReg170q9NebBAyiYrqCD2jn2Bpe47xMGpbzsPoVvoG3axEIth1AG6MAGwE');
+define('LOGGED_IN_KEY',    'VGx1b7GNTncWEntbRA4nPL3FF4VL1N8ogeqgCviEAHwxsnIia4uQMqT0haPFqbnP');
+define('NONCE_KEY',        'b9eHBAgWnuRXDTzaWE2X6JrgV9cRtIrFC1Ytc9ySJVftiGYPqHnA2XJJezzLJz10');
+define('AUTH_SALT',        'dUFaCs2GrcyD7L1qaTUitN5HmRJewuQitrn6xTv65cDv7fq4yq07seeVvvWCq1Sg');
+define('SECURE_AUTH_SALT', 'PgWvcpVCpzQrGqvsXyyE4HWhniAWEwm2HxErnUAGQrM2UwznB8VaWABnqwNAJJRm');
+define('LOGGED_IN_SALT',   'RUmGUJKtLcTfrFdx5vLL74TNgMJLXMVV8VYWLdLn4FH2NgGMNnp2zwqxLJpnrCjm');
+define('NONCE_SALT',       'Pghx3VGj8g2ILnzQUWvahIIL5rQL0Rdoj5hhnP6F0MAMPhCN6FK6uYUgMTRjYyG7');
